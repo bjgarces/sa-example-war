5cc743feb298aa46ccc2c32964b67fe5f9b2cc92
bjgarces <bjgarces@centilia.com.mx>
2017-10-19 14:50:41 -0500

    Nueva Calculadora

---
+++ src/main/java/com/bbva/sa_example_war/Calculadora.java
@@ -0,0 +1,24 @@
+package com.bbva.sa_example_war;
+
+public class Calculadora {
+
+	public static int sumar(int x, int y)
+	{
+		return x+y;
+	}
+	
+	public static int restar(int x, int y)
+	{
+		return x-y;
+	}
+	
+	public static int multiplicar(int x, int y)
+	{
+		return x*y;
+	}
+	
+	public static float dividir(int x, int y)
+	{
+		return x*y;
+	}
+}
---
+++ src/test/java/com/bbva/sa_example_war/CalculadoraTest.java
@@ -0,0 +1,23 @@
+package com.bbva.sa_example_war;
+
+import junit.framework.TestCase;
+
+public class CalculadoraTest extends TestCase {
+
+	public void testSumar() {
+		assertEquals(10, Calculadora.sumar(5, 5));
+	}
+
+	public void testRestar() {
+		assertEquals(0, Calculadora.restar(5, 5));
+	}
+
+	public void testMultiplicar() {
+		assertEquals(25, Calculadora.multiplicar(5, 5));
+	}
+
+	public void testDividir() {
+		assertEquals(1, Calculadora.dividir(5, 5));
+	}
+
+}
